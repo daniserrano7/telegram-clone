8a523acc659a59a4a83c55f791e02b7d1ee3e527
Daniel Serrano <danielserrano.contacto@gmail.com>
2024-09-15 00:31:05 +0200

    feat: auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const user = await this.authService.register(email, password);
+      return res.json(user);
+    } catch (error) {
+      return res
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json({ error: 'Server internal error' });
+    }
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const { token, user } = await this.authService.login(email, password);
+      return res.json({ token, user });
+    } catch (error) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ error: 'Invalid credentials' });
+    }
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,44 @@
+// src/auth/auth.service.ts
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { DbService } from 'src/db/db.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: DbService) {}
+
+  generateToken(userId: number) {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
+  }
+
+  async register(email: string, password: string) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) throw new Error('User already exists');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    const token = this.generateToken(newUser.id);
+    return { user: newUser, token };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new Error('Invalid credentials');
+    }
+
+    const token = this.generateToken(user.id);
+    return { token, user };
+  }
+}
