01e30085e983ea78a9a5f0a6da906926239dc7d7
Daniel Serrano <danielserrano.contacto@gmail.com>
2025-03-28 09:54:43 +0100

    feat: add VPS deployment

--- run-prod.sh
+++ run-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Stop any existing containers
-docker-compose down
+docker-compose -f docker-compose.prod.yml down
 
 # Remove the web_dist volume
 docker volume rm telegram-clone_web_dist
 
 # Start the production containers
-docker-compose --profile production up -d --build
+docker-compose -f docker-compose.prod.yml --profile production up -d --build
 
 # Show container logs
-docker-compose logs -f
+docker-compose -f docker-compose.prod.yml logs -f
